d1af79177ac16a89a840636209a0af62f41ff725
Kunfury-blep <65051061+Kunfury-blep@users.noreply.github.com>
2021-12-14 16:00:12 -0500

    Added command for creating a new contest      Automatically backsup the previous contest Bugfix for if someone left the server while in the leaderboard Bugfix for if someone does not have a set nickname, now defaults to their username

--- Commands/Modules/AdminModule.cs
+++ Commands/Modules/AdminModule.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Cluckin_Bot.Commands.Modules
@@ -13,27 +14,66 @@ namespace Cluckin_Bot.Commands.Modules
     {
         [Command("setName")]
         [RequireUserPermission(GuildPermission.BanMembers)]
-        public async Task ChangeMeal([Remainder] string args = null)
+        public async Task RenameMeal([Remainder] string args = null)
+        {
+            var sb = new StringBuilder();
+
+            if (args == null) sb.AppendLine("Please enter a name for the current meal");
+            else sb.AppendLine(RenameContest(args));
+
+            await ReplyAsync(sb.ToString());
+
+        }
+
+        [Command("SetupMeal")]
+        [Alias("sm")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task SetupNewMeal([Remainder] string args = null)
         {
             var sb = new StringBuilder();
 
             Console.WriteLine(args);
-
-            if (args == null) sb.AppendLine("Please enter a name for the current meal");
+            if (args == null) sb.AppendLine("Please enter a name for the new meal");
             else
             {
-                string json = File.ReadAllText("config.json");
-                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
-                jsonObj["MealName"] = args;
-                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
-                File.WriteAllText("config.json", output);
-                Console.WriteLine($"config.json changed to: {jsonObj["MealName"]}");
-                sb.AppendLine($"Meal Name successfully changed to: {jsonObj["MealName"]}");
-                Program.Config["MealName"] = args.ToString();
+                Directory.CreateDirectory(Program.Config["MealFolder"]);
+
+                string mealName = Program.Config["MealName"];
+
+                //Strips any illegal values from the file name
+                string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+                Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+                mealName = r.Replace(mealName, "");
+
+                //Creates a backup of the current contest, sets the name to the contest name
+                string copyPath = $"{Program.Config["MealFolder"]}/{mealName}.json";
+                File.Copy("data.json", copyPath, true);
+
+                //Reverts the data.json file to a blank state
+                File.Copy("baselineData.json", "data.json", true);
+
+                sb.AppendLine("Successfully backed up the current Contest.");
+                sb.AppendLine(RenameContest(args));
             }
 
             await ReplyAsync(sb.ToString());
 
+        }
+
+
+
+        private string RenameContest(string name)
+        {
+            string json = File.ReadAllText("config.json");
+            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            jsonObj["MealName"] = name;
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText("config.json", output);
+            Console.WriteLine($"config.json changed to: {jsonObj["MealName"]}");
+            
+            Program.Config["MealName"] = name.ToString();
+
+            return ($"Meal Name successfully changed to: {jsonObj["MealName"]}");
         }
     }
 }
--- Commands/Modules/LeaderboardModule.cs
+++ Commands/Modules/LeaderboardModule.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using System.Linq;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Cluckin_Bot.Commands.Modules
 {
@@ -34,61 +35,156 @@ namespace Cluckin_Bot.Commands.Modules
             embed.WithColor(new Color(0, 255, 0));
             embed.Title = $"{Program.Config["MealName"]} Rankings!";
 
-            string name = (Context.Message.Author as SocketGuildUser).Nickname;
-            if (name == null) name = Context.User.Username;
+            List<User> users = GetLeaderboard();
 
-            sb.Append(GetLeaderboard(0));
+            //TODO: Add count for sales and cooks alongside the total
+            int i = 1;
+            foreach (var u in users)
+            {
+                sb.AppendLine($"{i}. {u.GetName(Context.Guild)}: {u.total}");
+                i++;
+            }
 
             embed.Description = sb.ToString();
             await ReplyAsync(null, false, embed.Build());
         }
 
-
-
-
-
-        private StringBuilder GetLeaderboard(int pos)
+ 
+        [Command("leaderboardSign")]
+        [Alias("lbs")]
+        public async Task LeaderboardSign()
         {
-            string json = _dh.GetData("");
-            JObject jObj = (JObject)JsonConvert.DeserializeObject(json);
+            // I like using StringBuilder to build out the reply
+            var sb = new StringBuilder();
+
+            List<User> users = GetLeaderboard();
+
+            if(users.Count > 3) users.RemoveRange(3, users.Count - 3);
+
+            if (users.Count <= 0) sb.AppendLine("There are no sales so far");
+            else
+            {
+                //sb.Append($"{Program.Config["MealName"]} Rankings!");
+                sb.AppendLine("**Copy below to a sign.**");
+                sb.AppendLine("```");
+                sb.Append("<b>Specials Cooked/Sold</b>");
+                sb.Append(GetSign(users[0], 1));
+                if(users.Count >= 2) sb.Append(GetSign(users[1], 2));
+                if (users.Count >= 3) sb.Append(GetSign(users[2], 3));
+                sb.AppendLine("```");
+            }
+
+
+            await ReplyAsync(sb.ToString());
+        }
+
+        private StringBuilder GetSign(User user, int num)
+        {
             StringBuilder sb = new StringBuilder();
-
-            //Gets the seperate JArrays from the single JSON
-            JArray sellers = (JArray)jObj["SALES"];
-            JArray cookers = (JArray)jObj["COOKS"];
-
-            //Converts the JArrays to lists, possibly unneeded?
-            List<Sale> sales = JsonConvert.DeserializeObject<List<Sale>>(sellers.ToString());
-            List<Sale> cooks = JsonConvert.DeserializeObject<List<Sale>>(cookers.ToString());
-
-            //Combines the lists into a single dictionary, using ID as key
-            Dictionary<UInt64, int> totalDict = new Dictionary<UInt64, int>();
-            foreach(var s in sales)
-            {
-                totalDict.Add(s.id, s.value);
-            }
-            foreach (var c in cooks)
-            {
-                if (totalDict.ContainsKey(c.id)) totalDict[c.id] += c.value;
-                else totalDict.Add(c.id, c.value);
-            }
-
-            var sortedDict = from entry in totalDict orderby entry.Value descending select entry;
-
-
-            int i = 1;
-            foreach (var s in sortedDict)
-            {
-                //IUser user = Context.Client.GetUserAsync(i.Key).Result;
-                IGuildUser gUser = Context.Guild.GetUserAsync(s.Key).Result;
-                if(gUser != null) sb.AppendLine($"{i}. {gUser.Nickname} - {s.Value}");
-                i++;
-            }
+            sb.Append($"~n~{num}. {user.GetName(Context.Guild)}: {user.total}");
 
             return sb;
         }
+
+        private List<User> GetLeaderboard()
+        {
+            List<User> users = new List<User>();
+            //Gets the full JSON Object
+            JObject jObj = (JObject)JsonConvert.DeserializeObject(_dh.GetData(""));
+
+
+            //Gets the specified segment of the JSON as a JArray
+            JArray sellers = (JArray)jObj["SALES"];
+            List<Sale> sales = JsonConvert.DeserializeObject<List<Sale>>(sellers.ToString());
+
+            //Iterates through the sales, adding them to the users.
+            foreach(var s in sales)
+            {
+                User u = new User()
+                {
+                    id = s.id,
+                    sales = s.value
+                };
+                users.Add(u);
+            };
+
+            sellers = (JArray)jObj["COOKS"];
+            sales = JsonConvert.DeserializeObject<List<Sale>>(sellers.ToString());
+
+            //Iterates through the cooks, adding the values to the users, creating a new user if none exist
+            foreach (var s in sales)
+            {
+                bool exists = false;
+                foreach(var i in users)
+                {
+                    if (i.id == s.id)
+                    {
+                        i.cooks = s.value;
+                        i.total = i.cooks + i.sales;
+                        exists = true;
+                        break;
+                    }
+                    
+                }
+
+                if (!exists)
+                {
+                    User u = new User()
+                    {
+                        id = s.id,
+                        cooks = s.value,
+                        total = s.value
+                    };
+                    users.Add(u);
+                }
+            };
+
+            users.Sort();
+            users.Reverse();
+            return users;
+        }
+
     }
-    
+
+    public class User : IComparable<User>
+    {
+        public UInt64 id;
+        public int cooks;
+        public int sales;
+
+        public int total;
+
+        //Custom sorter based on the total values
+        public int CompareTo(User other)
+        {
+
+            return total.CompareTo(other.total);
+        }
+
+        /// <summary>
+        /// Gets the name of the specified user
+        /// </summary>
+        /// <param name="guild"></param>
+        /// <returns></returns>
+        public string GetName(IGuild guild)
+        {
+            string result = "*N/A*";
+            if (guild != null)
+            {
+                var user = guild.GetUserAsync(id);
+                if (user.Result != null)
+                {
+                    result = user.Result.Nickname;
+                    if (String.IsNullOrEmpty(result)) result = user.Result.Username;
+                }
+                    
+            }
+            
+               
+            return result;
+
+        }
+    }
 
 }
 
@@ -97,3 +193,4 @@ public class Sale
     public UInt64 id;
     public int value;
 }
+
